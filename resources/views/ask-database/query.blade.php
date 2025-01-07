516e2565705d286df40cc48a1c2b59880b3b8e9e
vijay-fs <vijaykarthik9868@gmail.com>
2025-01-07 14:44:43 +0530

    Dynamic queries set

--- resources/views/ask-database/query.blade.php
+++ resources/views/ask-database/query.blade.php
@@ -32,8 +32,41 @@ Guidelines:
 
 Question: "User question here"
 SQLQuery: "SQL Query used to generate the result (if applicable)"
-SQLResult: "Result of the SQLQuery (if applicable)"
-Answer: "Final answer here (You fill this in with the SQL query only)"
+SQLResult: "
+@if(!empty($result))
+    @if(is_array($result))
+        {{ json_encode($result, JSON_PRETTY_PRINT) }}
+    @elseif(is_string($result))
+        {{ $result }}
+    @else
+        No results found or query execution failed.
+    @endif
+@else
+    Query execution returned no results or encountered an error.
+@endif
+"
+Answer: "
+@if(!empty($result))
+    @if(is_array($result))
+        Final Answer: 
+        <ul>
+            @foreach ($result as $row)
+                <li>
+                    @foreach ($row as $key => $value)
+                        {{ $key }}: {{ $value }};
+                    @endforeach
+                </li>
+            @endforeach
+        </ul>
+    @elseif(is_string($result))
+        Final Answer: {{ $result }}
+    @else
+        No results found or query execution failed.
+    @endif
+@else
+    Sorry, no answer could be generated.
+@endif
+"
 
 ---
 
@@ -58,3 +91,13 @@ SQLResult: "@if($result){!! $result !!}"
 @else
 (Your answer HERE must be a syntactically correct MySQL query with no extra information or quotes. Omit SQLQuery: from your answer)
 @endif
+
+
+@php
+// Log the final rendered answer
+Log::info('Final Answer:', [
+    'question' => $question,
+    'query' => $query,
+    'result' => $result
+]);
+@endphp
